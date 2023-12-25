6cef029ef2fd74bb8b41d0a5e07d433d162e8556
statkevich <statkevich.ivan9999@gmail.com>
2023-12-25 15:51:35 +0200

    Implement user authentication for route navigation
    Added user authentication before rendering different routes in the application. By using redux useSelector, the application now retrieves the current user state and determines the navigation path based on whether the user is authenticated or not, hence enhancing route protection. The application now redirects unauthenticated users to the

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React, {Suspense, useEffect} from 'react';
 import {authRoutes, routes} from './routing/routes';
-import {Route, Routes} from 'react-router-dom';
-import ProtectedRoute from './routing/ProtectedRoute';
+import {Navigate, Outlet, Route, Routes} from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import {RootState} from './store/store';
+import {UserI} from './types/user';
+import paths from './routing/paths';
 
 function App() {
+  const currentUser = useSelector<RootState>(state => state.user.currentUser) as UserI;
 
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', 'light');
@@ -13,12 +17,14 @@ function App() {
     <Suspense>
       <Routes>
         {routes.map(route => (
-          <Route key={route.path} element={<ProtectedRoute/>}>
+          <Route key={route.path} element={currentUser ? <Outlet/> : <Navigate to={paths.signIn} replace/>}>
             <Route path={route.path} element={<route.element/>}/>
           </Route>
         ))}
         {authRoutes.map(route => (
-          <Route key={route.path} path={route.path} element={<route.element/>}/>
+          <Route key={route.path} element={!currentUser ? <Outlet/> : <Navigate to={paths.main} replace/>}>
+            <Route path={route.path} element={<route.element/>}/>
+          </Route>
         ))}
       </Routes>
     </Suspense>
