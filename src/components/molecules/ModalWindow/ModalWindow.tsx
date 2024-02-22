91275596c6f8f2867ea89e5646c0bd946ff9a612
statkevich <statkevich.ivan9999@gmail.com>
2024-02-22 10:39:00 +0200

    Add close icon to ModalWindow component
    A new SVG close icon has been added to the project assets and integrated into the ModalWindow component. The icon has been styled and positioned in the top-right corner of the modal. The icon also has hover and active states to improve user interaction. Now, users can close the modal window by clicking on the close icon, enhancing the user experience.

--- src/components/molecules/ModalWindow/ModalWindow.tsx
+++ src/components/molecules/ModalWindow/ModalWindow.tsx
@@ -1,19 +1,24 @@
 import React, {FC, ReactNode} from 'react';
 import styles from './ModalWindow.module.scss';
 import EssentialBlock from '../../atoms/EssentialBlock/EssentialBlock';
+import {ReactComponent as CloseIcon} from '../../../assets/icons/close.svg';
 
 type EssentialBlockProps = {
   children?: ReactNode;
   close: () => void;
 }
 
-const ModalWindow: FC<EssentialBlockProps> = ({children}) => {
+const ModalWindow: FC<EssentialBlockProps> = ({children, close}) => {
 
   return (
     <div className={styles.modalWindow}>
       <div className={styles.modalWindowContent}>
         <EssentialBlock>
           {children}
+          <CloseIcon
+            className={styles.closeIcon}
+            onClick={close}
+          />
         </EssentialBlock>
       </div>
     </div>
