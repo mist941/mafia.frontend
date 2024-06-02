dfda1556137a819769d47f9cc898c01c1b96ab52
statkevich <statkevich.ivan9999@gmail.com>
2024-06-02 16:06:07 +0300

    Implement redux store in chat message update
    The code implements the integration of Redux Store into the chat engine. The function `updateMessages` now includes not only session storage update, but also dispatches a newly added action `addMessage`, to ensure global state consistency.

--- src/utils/chat.ts
+++ src/utils/chat.ts
@@ -1,6 +1,9 @@
 import {Message} from '../types/chat';
+import store from '../store/store';
+import {addMessage} from '../store/chat/chat.reducer';
 
 export const updateMessages = (message: Message) => {
   const messages = JSON.parse(sessionStorage.getItem('messages') ?? '[]');
   sessionStorage.setItem('messages', JSON.stringify([...messages, message]));
+  store.dispatch(addMessage(message));
 }
