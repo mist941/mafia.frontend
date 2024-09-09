e4eb6fb3651ac1d8efce125f9bfbddd036d8a73f
statkevich <statkevich.ivan9999@gmail.com>
2024-09-09 11:23:29 +0300

    Refactor skip action button logic
    Moved the skip action logic in `SkipActionButton` component to a custom hook `useMakePlayerAction`. This improves code reuse and maintainability by abstracting the action logic into a separate hook.

--- src/components/molecules/SkipActionButton/SkipActionButton.tsx
+++ src/components/molecules/SkipActionButton/SkipActionButton.tsx
@@ -1,35 +1,10 @@
 import React from 'react';
 import Button from '../../atoms/Button/Button';
-import {useSelector} from 'react-redux';
-import {RootState} from '../../../store/store';
-import {selectCurrentGame} from '../../../store/game/game.selector';
-import {CurrentGame, GameResponse} from '../../../types/game';
-import {FetchResult, useMutation} from '@apollo/client';
-import {CREATE_ACTION} from '../../../graphql/game';
-import {updateCurrentGameData} from '../../../utils/game';
 import {ActionTypes} from '../../../types/action';
+import {useMakePlayerAction} from '../../../hooks/useMakePlayerAction';
 
 const SkipActionButton = () => {
-  const {player: currentPlayer, game} = useSelector<RootState>(selectCurrentGame) as CurrentGame;
-  const [createAction] = useMutation(CREATE_ACTION);
-
-  const skip = async () => {
-    const skipActionResponse: FetchResult<{ readyToPlay: GameResponse }> = await createAction({
-      variables: {
-        createActionInput: {
-          gameId: game.id,
-          playerId: currentPlayer.id,
-          actionType: ActionTypes.SKIP,
-          step: game.step,
-          period: game.currentPeriod
-        }
-      },
-    });
-
-    if (skipActionResponse.data?.readyToPlay) {
-      updateCurrentGameData(skipActionResponse.data?.readyToPlay);
-    }
-  }
+  const skip = useMakePlayerAction(ActionTypes.SKIP);
 
   return (
     <Button styled='secondary' onClick={skip}>
---
+++ src/hooks/useMakePlayerAction.tsx
@@ -0,0 +1,31 @@
+import {FetchResult, useMutation} from '@apollo/client';
+import {CREATE_ACTION} from '../graphql/game';
+import {CurrentGame, GameResponse} from '../types/game';
+import {ActionTypes} from '../types/action';
+import {updateCurrentGameData} from '../utils/game';
+import {useSelector} from 'react-redux';
+import {RootState} from '../store/store';
+import {selectCurrentGame} from '../store/game/game.selector';
+
+export const useMakePlayerAction = (action: ActionTypes) => {
+  const {player: currentPlayer, game} = useSelector<RootState>(selectCurrentGame) as CurrentGame;
+  const [createAction] = useMutation(CREATE_ACTION);
+
+  return async () => {
+    const skipActionResponse: FetchResult<{ readyToPlay: GameResponse }> = await createAction({
+      variables: {
+        createActionInput: {
+          gameId: game.id,
+          playerId: currentPlayer.id,
+          actionType: action,
+          step: game.step,
+          period: game.currentPeriod
+        }
+      },
+    });
+
+    if (skipActionResponse.data?.readyToPlay) {
+      updateCurrentGameData(skipActionResponse.data?.readyToPlay);
+    }
+  };
+}
